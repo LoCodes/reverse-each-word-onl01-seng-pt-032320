33a6188776543ae037576ec3a8c9faf2fb1386f3
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-08 03:31:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,11 @@ sentence2 = "Hi again, just making sure it's reversed!"
 def reverse_each_word(sentence1)
   sentence1.reverse.split.reverse.join(" ")
 end
+
+def reverse_each_word(sentence2)
+  sentence2 = string.split
+  sentence2.collect do |word|
+    word.reverse
+  end
+  sentence2.join("")
+end

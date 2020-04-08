7907b4861090ec5ff05b0aae32daaf744adb6483
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-08 02:35:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,7 @@
-def reverse_each_word("Hello there, and how are you?")
+sentence1 = "Hello there, and how are you?"
+sentence2 = "Hi again, just making sure it's reversed!" 
+
+
+def reverse_each_word()
   reverse_each_word.reverse 
 end

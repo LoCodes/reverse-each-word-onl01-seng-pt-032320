76554b0978d39f6f2c4fbdba3b4a32dee1a5ab11
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-08 03:49:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,9 +5,7 @@ sentence2 = "Hi again, just making sure it's reversed!"
 
 
 def reverse_each_word(sentence1)
-  sentence1.reverse{|word| word.split.join("")}
-
-  # .reverse.split.reverse.join(" ")
+  sentence1.reverse.split.reverse.join(" ")
 end
 
 .collect{|letter| letter.capitalize}

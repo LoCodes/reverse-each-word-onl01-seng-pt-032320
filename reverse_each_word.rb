f89529de009bfc118b1ff2d0973f333cc21be916
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-08 03:31:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,9 +4,9 @@ sentence1 = "Hello there, and how are you?"
 sentence2 = "Hi again, just making sure it's reversed!"
 
 
-def reverse_each_word(sentence1)
-  sentence1.reverse.split.reverse.join(" ")
-end
+# def reverse_each_word(sentence1)
+#   sentence1.reverse.split.reverse.join(" ")
+# end
 
 def reverse_each_word(sentence2)
   sentence2 = string.split
